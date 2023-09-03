1c2c4185cfbc7a8b8147b30f92edaf091796a302
JoJoWitness <Jordano470@gmail.com>
2023-09-03 13:09:51 -0400

    Two sum

---
+++ Arrays/EasyExercises/TwoSum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+
+        numsDict = {}
+
+        for i in range(len(nums)):
+            if (target -nums[i]) in numsDict:
+                return [numsDict[target - nums[i]], i]
+            else:
+                numsDict[nums[i]] = i
